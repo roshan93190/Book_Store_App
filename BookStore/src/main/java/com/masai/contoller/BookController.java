3621875c48a12c1f4db3753a37661c956a2a6eff
Roshan kumar <99637667+roshan93190@users.noreply.github.com>
2022-10-03 02:46:35 +0530

    project updated

--- BookStore/src/main/java/com/masai/contoller/BookController.java
+++ BookStore/src/main/java/com/masai/contoller/BookController.java
@@ -3,6 +3,8 @@ package com.masai.contoller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +34,10 @@ public class BookController {
 	}
 	
 	@GetMapping("bookservice/books")
-	public List<Book> getAllBooks(  ) {
+	public ResponseEntity<List<Book>> getAllBooks(  ) {
 		
-		return bookService.getAllBook();
+		List<Book> allBook =bookService.getAllBook();
+		return new ResponseEntity<List<Book>>(allBook,HttpStatus.OK);
 		
 	}
 	
